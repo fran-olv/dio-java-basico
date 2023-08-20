6ccab2fa48a1b129401541e5759c5d1dcd6c4a39
Franciele Batista <francielebatista@id.uff.br>
2023-08-19 22:05:41 -0300

    anatomia de classes e variáveis.

---
+++ java-anatomia-da-classe/java-anatomia-classes/src/Ensinamentos.java
@@ -0,0 +1,47 @@
+public class Ensinamentos {
+    public static void main(String[] args) {
+        System.out.println("Olá turma, sejam bem vindos!");
+    
+        int ano = 2021;
+        ano = 2022;
+        String BR = "Brasil"; //variavel que vai ser mudada ao longo do código
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+
+//Padrão de nomenclatura
+    // Declação inválida de variáveis
+
+       // int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+       //  int 1numero = 1;    //Uma variável não pode começar com númerico
+       // int numero um = 1; //Não pode ter espaço no nome da variável
+       // int long = 1; //long faz parte das palavras reservadas da linguagem
+        
+    // Declaração válida de veriáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
+
+    //Declarando variáveis e métodos
+        // Estrutura variáveis
+
+        Tipo NomeBemDefinido = Atribuicao (opcional em alguns casos)
+
+        // Exemplo variáveis
+
+        int idade = 23;
+        double altura = 1.62;
+        Dog spike; //observe que aqui a variável spike não tem valor, é normal
+        
+
+        //Estrutura mÉtodos
+
+        TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+        //Exemplo mÉtodos
+
+        int somar (int numeroUm, int numero2)
+
+        String formatarCep (long cep)
+            }
+        }
