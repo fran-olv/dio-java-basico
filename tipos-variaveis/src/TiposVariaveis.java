a5cc32a651c23dd1e12fbc6675f6607a9c7c711a
Franciele Batista <francielebatista@id.uff.br>
2023-08-20 22:08:34 -0300

    Tipos de variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+    int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+    int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+    double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+}
