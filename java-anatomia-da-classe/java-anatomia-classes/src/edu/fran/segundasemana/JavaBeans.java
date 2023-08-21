63b8b75e3835b88df96e4823977e9f4a41184f5d
Franciele Batista <francielebatista@id.uff.br>
2023-08-20 21:31:21 -0300

    Padronizacao javabeans de variaveis e metodos

---
+++ java-anatomia-da-classe/java-anatomia-classes/src/edu/fran/segundasemana/JavaBeans.java
@@ -0,0 +1,23 @@
+package edu.fran.segundasemana;
+public static void main(String[] args) {
+
+    //VARIAVEIS
+   /*  Não recomendado
+    double salMedio = 1500.23  //variável abreviada, o que dificulta a compreensão
+    String emails = "aluno@escola.com" //confuso se a variável seria um array ou único e-mail
+    String myName = "JOSEPH" //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não 
+   */
+  // Recomendado 
+    double salarioMedio=1500.23;
+    String email ="aluno@escola.com";
+    String [] emails = {"aluno@escola.com","professor@escola.com"}
+    String meuNome = "JOSEPH" 
+    
+    //METODOS
+    somar(int n1, int n2){}
+    abrirConexao(){}
+    concluirProcessamento() {}
+    findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+    calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+
+    }
