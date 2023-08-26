44c646e5582b20a326fa5bb8e35cf884da8e9553
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-26 16:22:07 -0300

    Inclusao de estruturas de repeticao

---
+++ estrutura-repeticao/ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+        
+            //Forma abreviada
+        for(String aluno : alunos) {
+          System.out.println(alunos);
+        }
+    
+    }
+    }
+    
+}
